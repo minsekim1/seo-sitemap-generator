803d836509d78ee5e467622016e19412818207e4
minsekim1 <tkarnrwl7862@naver.com>
2023-02-16 14:09:53 +0900

    Basic complted;2C

--- function/write.ts
+++ function/write.ts
@@ -1,4 +1,4 @@
 import fs from "fs";
 export const write = async (content: string, path = "./query.txt") =>
   await fs.writeFileSync(path, content, { flag: "a+" });
-export const reset = async () => await fs.writeFileSync("./query.txt", "", { flag: "a" });
+export const reset = async (path = "./query.txt") => await fs.writeFileSync(path, "", { flag: "w" });
--- index.ts
+++ index.ts
@@ -1,16 +1,26 @@
-import { write } from "./function/write";
-import XMLWriter from "xml-writer";
+import { reset, write } from "./function/write";
 
-const xml = new XMLWriter();
-xml.startDocument();
-xml.startElement("urlset");
-xml.writeAttribute("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
-xml.writeAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
-xml.writeAttribute(
-  "xsi:schemaLocation",
-  "http://www.sitemaps.org/schemas/sitemap/0.9            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-);
-xml.text("Some content");
-xml.endDocument();
+// 변수
+const isReset = true;
+const filepath = "sitemap.xml";
 
-write(xml.toString(), "sitemap.xml");
+// 파일 초기화
+if (isReset) {
+  reset(filepath);
+  // 기본 헤더 추가
+  const header = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n`;
+  write(header, filepath);
+}
+
+// 내용
+var xml = require("xml");
+const xmlString = xml({ url: [{ loc: "locasd" }, { priority: "1.01111" }] }, true);
+write(xmlString.toString(), filepath);
+
+// 마지막 Footer
+if (isReset) write("\n</urlset>", filepath);
